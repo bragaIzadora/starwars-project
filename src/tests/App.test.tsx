8f392e418a0b8d8a7aafb64df6a031671eb83209
Izadora Braga <izadora_abraga@outlook.com>
2024-01-17 19:17:11 -0300

    requisito 8

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import App from '../App';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import NumericFilters from '../components/starwarsNumericFilters';
 
 test('App data-testid', () => {
   render(<App />);
@@ -10,9 +11,24 @@ test('App data-testid', () => {
 
   expect(screen.getByTestId('button-filter')).toBeInTheDocument();
 
+  userEvent.selectOptions(screen.getByTestId('column-filter'), ['orbital_period']);
+  userEvent.selectOptions(screen.getByTestId('comparison-filter'), ['menor que']);
+  userEvent.type(screen.getByTestId('value-filter'), '200');
   userEvent.click(screen.getByTestId('button-filter'));
+
+  userEvent.click(screen.getByTestId('button-remove-filters'));
 
 });
 
+it('Testando filtros', () => {
+  render(<App />);
 
+  expect(screen.getByTestId('name-filter')).toBeInTheDocument();
+  fireEvent.change(screen.getByTestId('name-filter'), { target: { value: 'Naboo' } });
+  expect(screen.getByTestId('name-filter')).toHaveDisplayValue('Naboo');
+  
+  expect(screen.getByTestId('column-filter')).toBeInTheDocument();
+  fireEvent.change(screen.getByTestId('column-filter'), { target: { value: 'diameter' } });
+  expect(screen.getByTestId('column-filter')).toHaveDisplayValue('diameter');
 
+});
